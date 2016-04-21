5d9b3e00aea288322f5b7ea15c0082aaf71dddc6
hasbrour1-wk <roberthasbrouck@gmail.com>
2016-04-21 15:58:10 -0400

    Push 1
    Finished the basic GUI for the application.

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Track your favorite NHL Teams with this tracker
 
 This android app allows you to add NHL teams to track.  It will pop up alerts for the team activity.  It will use an NHL JSON api to 
-access information on the selected teams.  
+access information on the selected teams.  The website for the api is http://nhlwc.cdnak.neulion.com
---
+++ app/src/main/java/com/hasbrouckproductions/rhasbrouck/nhltracker/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.hasbrouckproductions.rhasbrouck.nhltracker;
+
+/*
+    The HomeActivity will be the main activity of NHLTracker.  It will list the added teams, a button
+    to add or remove teams, and a refresh button to refresh data for now.  I will remove the refresh
+    button eventually and auto refresh.
+
+ */
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+
+    //refreshData will get the data from
+    //http://nhlwc.cdnak.neulion.com/fs1/nhl/league/clubschedule/NYR/2016/04/iphone/clubschedule.json
+    //replace NYR with selected team code, and the date with current year and month.
+    public void refreshData(){
+
+    }
+}
